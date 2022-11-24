69767b84db71ca450d24b1d2608e223991101ce3
vvcaw <eric.elflein@googlemail.com>
2022-11-24 14:03:25 +0100

    Solved 07

---
+++ 07.hs
@@ -0,0 +1,7 @@
+data NestedList a = Elem a | List [NestedList a]
+
+flatten :: NestedList a -> [a]
+flatten (Elem x) = [x]
+flatten (List []) = []
+flatten (List [x]) = flatten x
+flatten (List (x:xs)) = (flatten x) ++ (flatten $ List xs)
