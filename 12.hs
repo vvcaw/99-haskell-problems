17c3863a17453a20856a46fd454056f5e1666d46
vvcaw <eric.elflein@googlemail.com>
2022-12-02 20:29:22 +0100

    Solved 12

---
+++ 12.hs
@@ -0,0 +1,6 @@
+data Encoded a = Multiple Int a | Single a deriving Show
+
+decodeModified :: [Encoded a] -> [a]
+decodeModified [] = []
+decodeModified ((Multiple c x):xs) = replicate c x ++ decodeModified xs
+decodeModified ((Single x):xs) = x : decodeModified xs
